3eba8a721caaf866a2e61c734dd729bf43bddcfc
notezjs <notez.js@outlook.com>
2022-02-13 16:31:51 +0100

    you can now upload a txt file, the text will display in the textarea, the most used word will also display, the most used word in the text will change to foo+<mostUsedWord>+bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,80 @@
-
-import './App.css';
-
+import React, { useState } from 'react';
+  
 function App() {
-  return (
-    <div>
+
+    const [state, setState] = useState('');
+    const [mestord, setMestord] = useState('');
+   
+    const upload = () => {
+        const input = document.getElementsByClassName('texten');
+        // const textarea = document.getElementsByClassName('visas');
+
+
+        let files =  input[0].files;
+
+        const file = files[0];
+        
+        let reader = new FileReader();
+
+    
+            reader.onload = (event) => {
+                const file = event.target.result;
+
+                const commonWord =  getMax(file);
+
+                const result = file.split(commonWord).join(`foo${commonWord}bar`);
+
+                setState({body: result});
+            }
+
+        reader.readAsText(file);
+    }
+
+
+    const getMax = (str) => {
+
+        var wordCounts = {};
+        var words = str.match(/[a-z'\-]+/gi);
+
+        for(var i = 0; i < words.length; i++) {
+            wordCounts[words[i]] = (wordCounts[words[i]] || 0) + 1;
+        }
+
+        var sortable = [];
+
+        for (var word in wordCounts) {
+            sortable.push([word, wordCounts[word]]);
+        }
+    
+        sortable.sort(function(a, b) {
+            return b[1] - a[1];
+        });
+
+        const mostCommonWord = sortable[0][0];
+        console.log(sortable)
+        
+
+        setMestord({body: mostCommonWord});
+        
+        return mostCommonWord;
+   };
+
+   const handleSubmit = (e) => e.preventDefault()
+  
+    return (
+      <div>
         <h1>React TextArea - highlight the most common  word with 'foo' + commonWord + 'bar'</h1>
-        <form >
-            <input className='texten' type="file" />
-            <textarea className='visas' cols="100" rows="30" 
-                    placeholder="text will appear here" >
-            </textarea>
-            <h2>Most used word is: </h2>
+        <form onSubmit={handleSubmit}>
+            <input className='texten' type="file" onChange={upload}/>
+                <textarea className='visas' cols="100" rows="30" 
+                        placeholder="text will appear here" value={state.body}>
+                </textarea>
+                <h2>Most used word is: {mestord.body}</h2>
         </form>
-    </div>
-  );
+      </div>
+    );
+  
 }
 
+  
 export default App;
