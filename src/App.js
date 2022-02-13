9217017b286c0bfa526b6b286a5b3d8aa03bcbbc
notezjs <notez.js@outlook.com>
2022-02-13 12:53:22 +0100

    added the structure of the site, file upload and textarea

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+        <h1>React TextArea - highlight the most common  word with 'foo' + commonWord + 'bar'</h1>
+        <form >
+            <input className='texten' type="file" />
+            <textarea className='visas' cols="100" rows="30" 
+                    placeholder="text will appear here" >
+            </textarea>
+            <h2>Most used word is: </h2>
+        </form>
     </div>
   );
 }
